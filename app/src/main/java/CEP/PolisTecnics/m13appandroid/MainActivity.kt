bda4c89f0e4f1368b9d4408e9c83d05172f6735d
MarcOrobitgB <148786617+MarcOrobitgB@users.noreply.github.com>
2023-11-08 18:44:31 +0100

    primer pasar fet

--- app/src/main/java/CEP/PolisTecnics/m13appandroid/MainActivity.kt
+++ app/src/main/java/CEP/PolisTecnics/m13appandroid/MainActivity.kt
@@ -1,11 +1,21 @@
 package CEP.PolisTecnics.m13appandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val botonpasar1a2: TextView = findViewById(R.id.botonpasar1a2)
+        botonpasar1a2.setOnClickListener {
+            // Crear un Intent para abrir Activity2
+            val intent = Intent(this@MainActivity, Activity2::class.java)
+            startActivity(intent)
+        }
     }
 }
