b0175a75e1aaace1c406e6e758a73e6b8b63e86e
MarcOrobitgB <148786617+MarcOrobitgB@users.noreply.github.com>
2023-11-08 19:51:47 +0100

    cambis pasar i torna ej 5 fet

--- app/src/main/java/CEP/PolisTecnics/m13appandroid/Activity5.kt
+++ app/src/main/java/CEP/PolisTecnics/m13appandroid/Activity5.kt
@@ -1,11 +1,28 @@
 package CEP.PolisTecnics.m13appandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity5)
+
+
+        val botonatras: TextView = findViewById(R.id.botonatras)
+        val btnJugar: TextView = findViewById(R.id.btnJugar)
+
+        botonatras.setOnClickListener {
+            // Crear un Intent para abrir Activity2
+            val intent = Intent(this@Activity5, Activity4::class.java)
+            startActivity(intent)
+        }
+        btnJugar.setOnClickListener {
+            // Crear un Intent para abrir Activity2
+            val intent = Intent(this@Activity5, Activity6::class.java)
+            startActivity(intent)
+        }
     }
 }
