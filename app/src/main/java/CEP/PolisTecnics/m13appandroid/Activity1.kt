f9f649c157ff83462f22726c6f6a5217bc942101
ASBcep <asaboritb2324@politecnics.barcelona>
2023-11-10 18:04:07 +0100

    prova de canvi d'estil des de fitxer dimensions_frame.xml a activity 1

--- app/src/main/java/CEP/PolisTecnics/m13appandroid/Activity1.kt
+++ app/src/main/java/CEP/PolisTecnics/m13appandroid/Activity1.kt
@@ -1,11 +1,19 @@
 package CEP.PolisTecnics.m13appandroid
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity1 : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1)
+
+        val act1FrameText = findViewById<TextView>(R.id.act1FrameText)
+        act1FrameText.setTextAppearance(R.layout.dimensions_frame)
     }
 }
