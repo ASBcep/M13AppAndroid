1cee61d2e129ea7b66123de0f1bca484a22d9171
MarcOrobitgB <148786617+MarcOrobitgB@users.noreply.github.com>
2023-11-10 17:53:34 +0100

    activity 8 echo

---
+++ app/src/main/java/CEP/PolisTecnics/m13appandroid/Activity8.kt
@@ -0,0 +1,11 @@
+package CEP.PolisTecnics.m13appandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_8)
+    }
+}
