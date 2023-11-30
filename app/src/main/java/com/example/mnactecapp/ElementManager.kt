65b0ed55b43bf2bd1a6e14489e42901f8bd274d2
ASBcep <asaboritb2324@politecnics.barcelona>
2023-11-30 21:41:03 +0100

    1r commit

---
+++ app/src/main/java/com/example/mnactecapp/ElementManager.kt
@@ -0,0 +1,11 @@
+package com.example.mnactecapp
+
+
+object ElementManager {
+    //llistat d'objectes Element per a accedir-hi des de totes les classes
+    val elements = mutableListOf<Element>()
+    //Índex per al llistat elements, per localitzar un Element en particular (p.e. per mostrar a la pantalla de detalls del vehicle)
+    val indexElements = Int
+    //Definició d'idioma: 0 cat, 1 spa, 2 eng
+    val idioma = Int
+}
