c66fc0dc6d769a5c31ada94a9f366763bc2376de
ASBcep <asaboritb2324@politecnics.barcelona>
2023-12-09 22:52:33 +0100

    ConfigAdmin: programada càrrega json en tots els idiomes. Començo la programació de la carpeta imgelements, que permetrà afegir les imatges associades a cada element

--- app/src/main/java/com/example/mnactecapp/ConfigAdmin.kt
+++ app/src/main/java/com/example/mnactecapp/ConfigAdmin.kt
@@ -18,9 +18,10 @@ import java.io.InputStream
 import java.io.InputStreamReader
 
 class ConfigAdmin : AppCompatActivity() {
-    //variable per poder gestionar el json manualment quan es tria manualment
+    //variable per poder gestionar el json quan es tria manualment
     private val FILE_PICK_REQUEST_CODE_jsonElements = 5766//JSON
     private var originalLang = -1
+    private var jsonLang = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.config_admin)
@@ -31,22 +32,20 @@ class ConfigAdmin : AppCompatActivity() {
 
         //segons idioma canvio el valor global d'idioma per poder executar la lectura del json; després es restaura
         btnCat.setOnClickListener {
+
             originalLang = ElementManager.idioma
-            ElementManager.idioma = 0
+            jsonLang = 0
             btnLoadJson()
-            ElementManager.idioma = originalLang
         }
         btnSpa.setOnClickListener {
             originalLang = ElementManager.idioma
-            ElementManager.idioma = 1
+            jsonLang = 1
             btnLoadJson()
-            ElementManager.idioma = originalLang
         }
         btnEng.setOnClickListener {
             originalLang = ElementManager.idioma
-            ElementManager.idioma = 2
+            jsonLang = 2
             btnLoadJson()
-            ElementManager.idioma = originalLang
         }
     }
     //private fun btnLoadJson(originalLang: Int)
@@ -68,14 +67,12 @@ class ConfigAdmin : AppCompatActivity() {
         //ElementsList(this)
         //ElementManager.idioma = originalLang
     }
-    //Obrir arxiu json manualment en androids antics part 1
+    //obtindre ruta de l'arxiu json
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?, ) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == FILE_PICK_REQUEST_CODE_jsonElements && resultCode == Activity.RESULT_OK) {
             val uri = data?.data
-            // Aquí pots gestionar la URI de l'arxiu seleccionat
-            // Per exemple, pots llegir l'arxiu, processar-lo, etc.
             gestionarArxiuSeleccionat(uri)
         }
     }
@@ -87,7 +84,7 @@ class ConfigAdmin : AppCompatActivity() {
 
             //nom de l'arxiu segons idioma
             var fileName = "delete_me"
-            when (ElementManager.idioma){
+            when (jsonLang){
                 0 -> {fileName = "elements_cat.json"}
                 1 -> {fileName = "elements_spa.json"}
                 2 -> {fileName = "elements_eng.json"}
@@ -95,39 +92,67 @@ class ConfigAdmin : AppCompatActivity() {
             // Crear un nou fitxer de destinació on es copiaran les dades
             val destinationFile = File(filesDir, ("/json/" + fileName))
 
-            //comprobar i crear si fa falta la carpeta json
+            //comprovar i crear si fa falta la carpeta json
+            val jsonFolder = folderCheck("json")
+            /*
             val FolderJson = File(filesDir, "json")
             if (!FolderJson.exists()) {
                 val CreatedFolder = FolderJson.mkdir()
                 if (CreatedFolder) {
-                    // Carpeta creada amb èxit
-                    // Pots afegir aquí qualsevol lògica addicional si cal
-                    Toast.makeText(this,"carpeta json creada", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this,"Carpeta json creada", Toast.LENGTH_LONG).show()
                 } else {
-                    // Error en crear la carpeta
-                    Toast.makeText(this,"carpeta json no creada i no existent", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this,"Carpeta json no creada i no existent", Toast.LENGTH_LONG).show()
                 }
             } else {
-                // La carpeta ja existeix
-                Toast.makeText(this,"carpeta json ja existia", Toast.LENGTH_LONG).show()
-            }
+                Toast.makeText(this,"La carpeta json ja existia", Toast.LENGTH_LONG).show()
+            }*/
 
             // Copiar les dades de l'arxiu seleccionat al fitxer de destinació
-            try {
-                inputStream?.use { input ->
-                    FileOutputStream(destinationFile).use { output ->
-                        input.copyTo(output)
+            if(jsonFolder) {
+                try {
+                    inputStream?.use { input ->
+                        FileOutputStream(destinationFile).use { output ->
+                            input.copyTo(output)
+                        }
+                        Toast.makeText(this, "Fitxer copiat: " + destinationFile, Toast.LENGTH_LONG)
+                            .show()
+                        ElementManager.idioma = jsonLang
+                        ElementsList(this)
                     }
-                    Toast.makeText(this,"Fitxer copiat: " + destinationFile, Toast.LENGTH_LONG).show()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Toast.makeText(
+                        this,
+                        "Error !! Fitxer no copiat: " + destinationFile,
+                        Toast.LENGTH_SHORT
+                    ).show()
 
-                    ElementsList(this)
                 }
-            }catch (e: Exception){
-                e.printStackTrace()
-                Toast.makeText(this,"Error !! Fitxer no copiat: " + destinationFile, Toast.LENGTH_SHORT).show()
+                //comprovo si existeix o creo la carpeta imgelements, per emmagatzemar les imatges dels elements
+                val imgElementsFolder = folderCheck("imgelements")
+                if (imgElementsFolder)
+                {
 
+                }
             }
+            //retorno l'idioma en què es trobava l'app a l'índex
             ElementManager.idioma = originalLang
+        }
+    }
+    private fun folderCheck(folder: String): Boolean {
+        val FolderPath = File(filesDir, folder)
+        if (!FolderPath.exists()) {
+            val CreatedFolder = FolderPath.mkdir()
+            if (CreatedFolder) {
+                Toast.makeText(this,"Carpeta " + folder + " creada", Toast.LENGTH_LONG).show()
+                return true
+            } else {
+                Toast.makeText(this,"Carpeta " + folder + " no creada i no existent", Toast.LENGTH_LONG).show()
+                return false
+            }
+        } else {
+            Toast.makeText(this,"La carpeta " + folder + " ja existia", Toast.LENGTH_LONG).show()
+            return true
         }
     }
 }
