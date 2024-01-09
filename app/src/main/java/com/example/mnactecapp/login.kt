34cafec70e7c700be2a466d6cb96724b533ef995
MarcOrobitgB <148786617+MarcOrobitgB@users.noreply.github.com>
2024-01-09 19:23:43 +0100

    admin 1234

--- app/src/main/java/com/example/mnactecapp/login.kt
+++ app/src/main/java/com/example/mnactecapp/login.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,24 @@ class login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val btnAcceder: Button = findViewById(R.id.btnAcceder)
+        val editTextNombreUsuario: EditText = findViewById(R.id.editTextNombreUsuario)
+        val editTextContraseña: EditText = findViewById(R.id.editTextContraseña)
 
         btnAcceder.setOnClickListener{
 
-            val intent = Intent(this, ConfigAdmin::class.java)
-            startActivity(intent)
+            val nom = editTextNombreUsuario.text.toString()
+            val contra = editTextContraseña.text.toString()
+
+            if (nom == "admin" && contra == "1234") {
+                val intent = Intent(this, ConfigAdmin::class.java)
+                startActivity(intent)
+            } else {
+                mostrarToast("Nom o contrasenya incorrectes")
+            }
         }
     }
 
+    private fun mostrarToast(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+    }
 }
